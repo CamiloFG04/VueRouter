b18d93f4373b98a131caccaec87bcdbdd14175f3
Camilo Fernandez <andrescamilo592@gmail.com>
2023-09-28 18:06:26 -0500

    init

---
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Pokemon <span>#{{id}}</span></h1>
+</template>
+
+<script>
+export default {
+    props: {
+        id: {
+            type:Number,
+            required: true
+        }
+    },
+    data(){
+        return{ 
+            // id: null
+        }
+    },
+    created(){
+        // const {id} = this.$route.params;
+        // // this.id = id
+        // console.log(id);
+    }
+}
+</script>
